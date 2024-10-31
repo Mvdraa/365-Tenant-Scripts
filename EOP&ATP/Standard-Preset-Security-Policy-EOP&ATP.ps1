d2535aa60e76a691adaf20fbfd36bd18de59ade7
Mvdraa <68705299+Mvdraa@users.noreply.github.com>
2024-10-31 21:59:48 +0100

    Update Standard-Preset-Security-Policy-EOP&ATP.ps1

--- EOP&ATP/Standard-Preset-Security-Policy-EOP&ATP.ps1
+++ EOP&ATP/Standard-Preset-Security-Policy-EOP&ATP.ps1
@@ -1,4 +1,17 @@
 
+#Default file types Malware Filter
+$Malwarefiletypes = @("ace",
+"ani",
+"app",
+"docm",
+"exe",
+"jar",
+"reg",
+"scr",
+"vbe",
+"vbs")
+
+
 #ATPBuiltInProtectionRule
 New-SafeAttachmentPolicy -Name "Baseline | Standard Safe Attachment" -Action Block -Enable $true
 New-SafeLinksPolicy -Name "Baseline | Standard Safe Links" -AllowClickThrough $true -DeliverMessageAfterScan $true -DisableUrlRewrite $true -ScanUrls $true -TrackClicks $true -EnableSafeLinksForEmail $true -EnableSafeLinksForOffice $true
@@ -8,4 +21,20 @@ New-HostedContentFilterPolicy -Name "Baseline | Standard Hosted Content Filter" 
 
 New-AntiPhishPolicy -Name "Baseline | Standard Anti Phish" -AuthenticationFailAction MoveToJmf -DmarcQuarantineAction Quarantine -DmarcRejectAction Reject -EnableMailboxIntelligence $true -EnableMailboxIntelligenceProtection $true -EnableOrganizationDomainsProtection $true -EnableSimilarUsersSafetyTips $true -EnableSimilarDomainsSafetyTips $true -EnableSpoofIntelligence $true -EnableTargetedDomainsProtection $true -EnableTargetedUserProtection $true -EnableUnauthenticatedSender $true -EnableUnusualCharactersSafetyTips $true -EnableViaTag $true -HonorDmarcPolicy $true -ImpersonationProtectionState Automatic -MailboxIntelligenceProtectionAction MoveToJmf -MailboxIntelligenceQuarantineTag DefaultFullAccessPolicy -PhishThresholdLevel 2 -SpoofQuarantineTag DefaultFullAccessPolicy -TargetedDomainQuarantineTag DefaultFullAccessWithNotificationPolicy -TargetedUserQuarantineTag DefaultFullAccessWithNotificationPolicy 
 
-New-MalwareFilterPolicy -Name "Baseline | Standard Malware Filter" -QuarantineTag AdminOnlyAccessPolicy -ZapEnabled $true 
+New-MalwareFilterPolicy -Name "Baseline | Standard Malware Filter" -QuarantineTag AdminOnlyAccessPolicy -ZapEnabled $true -FileTypes $Malwarefiletypes
+
+
+
+
+#Enable Rules for Policy
+
+New-SafeAttachmentRule -Name "Baseline | Standard Anti Phish" -SafeAttachmentPolicy "Baseline | Standard Safe Attachment" -RecipientDomainIs $DOMAIN
+
+New-SafeLinksRule -Name "Baseline | Standard Safe Links" -SafeLinksPolicy "Baseline | Standard Safe Links" -RecipientDomainIs
+
+New-HostedContentFilterRule -Name "Baseline | Standard Hosted Content Filter" -HostedContentFilterPolicy "Baseline | Standard Hosted Content Filter" -RecipientDomainIs $DOMAIN
+
+New-AntiPhishRule -Name "Baseline | Standard Anti Phish" -AntiPhishPolicy "Baseline | Standard Anti Phish" -RecipientDomainIs $DOMAIN
+
+New-MalwareFilterRule -Name "Baseline | Standard Malware Filter" -MalwareFilterPolicy "Baseline | Standard Malware Filter" -RecipientDomainIs $DOMAIN
+
